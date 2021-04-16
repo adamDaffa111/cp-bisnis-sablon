a76d6e323caa9e8113b9c6773ed757288a6be857
adamDaffa111 <adamdaffa59@gmail.com>
2021-04-16 08:52:27 +0700

    update project

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,3 +2,19 @@
 document.querySelector('.toggle-btn').addEventListener('click',(e) => {
   e.target.parentElement.previousElementSibling.classList.toggle('show-menu');
 });
+
+
+// video popup
+const video = document.querySelector('.preview-video');
+
+document.querySelector('.play-btn').addEventListener('click',() => {
+  document.querySelector('.popup-video').classList.add('show');
+  video.play();
+  document.querySelector('.popup-video').addEventListener('click',(e) => {
+    if (e.target.className != 'preview-video') {
+      document.querySelector('.popup-video').classList.remove('show');
+      video.pause();
+    }
+  });
+});
+
